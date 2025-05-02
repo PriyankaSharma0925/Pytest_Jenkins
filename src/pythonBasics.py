2b74095b75e6ecfd78488c64313d3e1181d7ffc9
PriyankaSharma0925 <priya.sh2010@gmail.com>
2025-05-02 12:58:27 -0700

    Moving Requirement file from test_script folder to Project folfer

--- src/pythonBasics.py
+++ src/pythonBasics.py
@@ -10,9 +10,21 @@ class Basics:
 
     def basicFunc(self, a):
         new_value=a+ self.firstValue + self.base_var
+        Basics.base_var=new_value
         return new_value
 
+    def basicDic(self,**kwargs):
+        print(kwargs.items())
+        print(kwargs)
 
+    def base_varfun(self):
+        print("Print Base Variable's new value")
+        return self.base_var
 
 obj=Basics(10,20)
-print(obj.basicFunc(10))
+obj.basicFunc(100)
+a=obj.base_varfun()
+print(a)
+
+#print(obj.basicDic(a=2,b=3,c=4,d=5))
+
