e82f2ad027990a99a48bb73b8624eb820a46fee7
PriyankaSharma0925 <priya.sh2010@gmail.com>
2025-05-03 14:51:13 -0700

    Revert "Added 1 module to BAnk_details"
    This reverts commit 9a281b0fe429caf4aeb271afa339407d3189ea42.

--- test_scripts/test_BankAccount.py
+++ test_scripts/test_BankAccount.py
@@ -1,16 +1,6 @@
 import pytest
 from src.BankDetails import BankDetails
 
-class Test_BankDetails:
-
-    def __init__(self,lastname,firstname,email):
-        self.lastname = lastname
-        self.firstname = firstname
-
-
-    def BankDetails_Summary():
-        pass
-
-
+# class Test_BankDetails:
 
 
