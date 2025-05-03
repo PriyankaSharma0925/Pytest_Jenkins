9a281b0fe429caf4aeb271afa339407d3189ea42
PriyankaSharma0925 <priya.sh2010@gmail.com>
2025-05-03 14:40:54 -0700

    Added 1 module to BAnk_details

--- test_scripts/test_BankAccount.py
+++ test_scripts/test_BankAccount.py
@@ -1,6 +1,16 @@
 import pytest
 from src.BankDetails import BankDetails
 
-# class Test_BankDetails:
+class Test_BankDetails:
+
+    def __init__(self,lastname,firstname,email):
+        self.lastname = lastname
+        self.firstname = firstname
+
+
+    def BankDetails_Summary():
+        pass
+
+
 
 
