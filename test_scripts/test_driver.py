b46dbe494756a8599cee12485fe885952f89e6cc
Priyanka Sharma <92665462+PriyankaSharma0925@users.noreply.github.com>
2025-05-10 18:53:48 -0700

    Update test_driver.py

--- test_scripts/test_driver.py
+++ test_scripts/test_driver.py
@@ -7,6 +7,3 @@ def load_driver(browser_name):
     print(f"Driver name is {driver}")
     return driver
 
-
-def test_driver():
-    assert load_driver("Chrom") in ["Chrome", "Firefox", "Edge"], "Yes they are in List"
